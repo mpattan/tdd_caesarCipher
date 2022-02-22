caf9a40c21a1270bd147fd58a5774f16cd33ea28
mpattan <mpattan@asu.edu>
2022-02-21 21:06:09 -0700

    Added decrypt function unit test

--- test.py
+++ test.py
@@ -14,6 +14,10 @@ class TestDrivenDev(unittest.TestCase):
     def test_encrypt(self):
         self.assertEqual(mycipher.encrypt(self.string_input,mycipher.get_cipher_value(self.cipher_input)), 'nkrru') 
 
+    #to test decrypt function
+    def test_decrypt(self):
+        self.assertEqual(mycipher.decrypt(self.string_input,mycipher.get_cipher_value(self.cipher_input)), 'hello')
+        
 if __name__ == '__main__':
 
     tdd = TestDrivenDev()
@@ -21,5 +25,7 @@ if __name__ == '__main__':
     # run all the tests
     if tdd.action == 'encrypt' :
         tdd.test_encrypt()
+    else :
+        tdd.test_decrypt()
     # status
     print('### ALL TESTS PASSED ###')
