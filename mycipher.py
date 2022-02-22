a295956d877e5994330a4a4628e2c52789002e92
mpattan <mpattan@asu.edu>
2022-02-21 21:48:07 -0700

    Added function to decrypt

--- mycipher.py
+++ mycipher.py
@@ -13,7 +13,14 @@ def encrypt(string_input, cipher):
             string_output += chr(((ord(char)-97)+cipher)%26+97)
     return string_output
 
-
+def decrypt(string_input, cipher):
+    string_output = ""
+    for char in string_input:
+        if char.isupper():
+            string_output += chr(((ord(char)-65)-cipher)%26+65)
+        else:
+            string_output += chr(((ord(char)-97)-cipher)%26+97)
+    return string_output
 
 action = sys.argv[1]
 string_input = sys.argv[2]
@@ -23,5 +30,7 @@ cipher = get_cipher_value(cipher_input)
 
 if action == 'encrypt':
     output = encrypt(string_input, cipher)
+elif action == 'decrypt':
+    output = decrypt(string_input, cipher)    
 
 print(output)
